3d6fa5f82e6d9f2f508642c5ad4fc5a1fd127713
Lubabalo Lalapi <81327854+LubabaloLalapi@users.noreply.github.com>
2022-08-29 08:55:48 +0200

    Add files via upload

---
+++ MyObject.java
@@ -0,0 +1,10 @@
+package oops;
+//Create an object called "Obj" and print the value of x
+public class MyObject {
+    int x=5;
+
+    public static void main(String[]args){
+        MyObject obj=new MyObject();
+        System.out.println(obj.x);
+    }
+}
