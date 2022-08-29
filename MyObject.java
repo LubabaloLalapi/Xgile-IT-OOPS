9494858d8d188daf67dac447aa6b41bee38bcdbf
Lubabalo Lalapi <81327854+LubabaloLalapi@users.noreply.github.com>
2022-08-29 09:23:36 +0200

    Update MyObject.java

--- MyObject.java
+++ MyObject.java
@@ -4,7 +4,7 @@ public class MyObject {
     int x=5;
 
     public static void main(String[]args){
-        MyObject obj=new MyObject();
-        System.out.println(obj.x);
+        MyObject obj=new MyObject();//Creating the object
+        System.out.println(obj.x);//Prints the value of x, which is 5
     }
 }
